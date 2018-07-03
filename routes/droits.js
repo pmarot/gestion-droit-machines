e9682b8d811bfe95ad3006d39351caaf64850dba
pmarot <apmarot@gmail.com>
2018-07-03 15:23:40 +0400

    jointure collection efféctué defaut dans l'affichage collection droit

--- routes/droits.js
+++ routes/droits.js
@@ -6,6 +6,19 @@ var droit = require("../controllers/DroitController");
 //recuperer les droits
 router.get("/", droit.list);
 
+//voir un droit par son id
+router.get("/show/:id", droit.show);
 
+//cree un droit
+router.get("/create", droit.create);
+
+//sauvegarder un droit. /!\ cest un POST 
+router.post("/save", droit.save);
+
+//editer une droit
+router.get("/edit/:id", droit.edit);
+
+//edit update.  /!\ cest un POST 
+router.post("/update/:id", droit.update);
 //export du module router
 module.exports = router;
