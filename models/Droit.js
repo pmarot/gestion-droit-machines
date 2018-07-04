2a83ecc9fc15b5d446a74f6bb103294a8110b225
pmarot <apmarot@gmail.com>
2018-07-04 10:31:21 +0400

    rectification erreur mineur dans le code

--- models/Droit.js
+++ models/Droit.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DroitSchema = new mongoose.Schema({
-    id_utilisateur: [{ type:Schema.Types.ObjectId, ref: 'Utilisateur' }],
-    id_machine: [{ type: Schema.Types.ObjectId, ref: 'Machine' }],
-    // id_utilisateur: { type:String , ref: 'Utilisateur' },
-    // id_machine: { type: String, ref: 'Machine' },
+    // id_utilisateur: [{ type:Schema.Types.ObjectId, ref: 'Utilisateur' }],
+    // id_machine: [{ type: Schema.Types.ObjectId, ref: 'Machine' }],
+    id_utilisateur: { type:String , ref: 'Utilisateur' },
+    id_machine: { type: String, ref: 'Machine' },
 });
 
 module.exports = mongoose.model("Droit", DroitSchema);
